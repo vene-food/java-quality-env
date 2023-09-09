48533493fe023e3e13465a3579a2329f7302a271
Santiago Otálvaro <sotalvaroo1999@gmail.com>
2023-09-08 23:55:17 -0500

    teste

--- child-container/src/main/java/com/java/quality/env/Main.java
+++ child-container/src/main/java/com/java/quality/env/Main.java
@@ -1,6 +1,8 @@
 package com.java.quality.env;
 
 public class Main {
+
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
---
+++ child-container/src/test/java/com/java/quality/env/MainTest.java
@@ -0,0 +1,19 @@
+package com.java.quality.env;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void test(){
+        assertEquals("a","b");
+    }
+
+}
