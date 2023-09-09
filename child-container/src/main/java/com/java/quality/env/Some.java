db2f45edb38b8237cef3feb8884aebcfe4baaa3c
Santiago Otálvaro <sotalvaroo1999@gmail.com>
2023-09-09 12:21:07 -0500

    tests

--- child-container/src/main/java/com/java/quality/env/Some.java
+++ child-container/src/main/java/com/java/quality/env/Some.java
@@ -2,11 +2,20 @@ package com.java.quality.env;
 
 public class Some {
 
-    public int sum(int a, int b){
-        return a+b;
+    public int sum(int a, int b) {
+        return a + b;
     }
-    public int resta(int a, int b){return a-b;}
-    public int mult(int a, int b){return a*b;}
-    public float div(int a, int b){return (b!=0)?a/b:-1;}
+
+    public int resta(int a, int b) {
+        return a - b;
+    }
+
+    public int mult(int a, int b) {
+        return a * b;
+    }
+
+    public float div(int a, int b) {
+        return (b != 0) ? a / b : -1;
+    }
 
 }
